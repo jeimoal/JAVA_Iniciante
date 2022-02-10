ae7987fc50706de973c13a4971e695f88c26bc57
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-02-10 20:59:42 -0300

    Create repeticao3.java

---
+++ repeticao3.java
@@ -0,0 +1,14 @@
+package repeticao3;
+public class Repeticao3 {
+    public static void main(String[] args) {        
+        // Repetição crescente
+        /*for (int cp=1 ;cp<=10 ;cp++) {
+         *   System.out.println(cp + " Camabalhota(s)");
+        }*/
+        
+        //Repetição descrescente, saltando 2.
+        for (int cp = 10; cp >=1; cp-=2) {
+            System.out.println(cp + " Repetição(ões)");
+        }
+    }
+}
