c0bdbd358c551dba39c4cd85632b5f2f0c70604c
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-02-13 18:36:32 -0300

    Update AnoMesDia.java
    Alteração para identificar se o ano é bissexto

--- AnoMesDia.java
+++ AnoMesDia.java
@@ -1,17 +1,27 @@
 package mesesanos;
 
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MesesAnos {
     
     public static void main(String[] args) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy");
-        String mes[] = {"Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"};
-        int dia[] = {31,28,31,30,31,30,31,31,30,31,30,31};
-        for (int cont = 0; cont<mes.length; cont++) {            
-            System.out.print("O ano atual é " + dtf.format((ZonedDateTime.now())) + " e ");
-            System.out.println("o mês de " + mes[cont] + " têm " + dia[cont] + " dias.");
-        } 
-    }    
+        Calendar cal =  GregorianCalendar.getInstance(); //Classe para importar o calendário Gregoriano
+        
+        int ano = cal.get(Calendar.YEAR); //Criação da Variável e escolha da Sublassse para escolha do Ano
+        String mes[] = {"Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"}; //Criação do Vetor String para os meses
+        int dia[] = {31,28,31,30,31,30,31,31,30,31,30,31}; //Criação do Vetor inteiro para os dias
+        
+        //Verifica se o ano é bissexto
+        if ((ano%4 == 0) && (ano%100 != 0) || (ano%400 == 0)){
+                dia[1] = 29; //Atribui ao mês de FEV 29 dias.
+                System.out.println("O ano é Bissexto."); //Informa ao usuário que o ano é bissexto
+            }
+        
+        //Estrutura de repetição informando o ano, mês e dia.
+        for (int cont = 0; cont<mes.length; cont++) {
+            System.out.print("O ano atual é " + ano + " e "); //Escreve o ano no console
+            System.out.println("o mês de " + mes[cont] + " têm " + dia[cont] + " dias."); //Escreve o mes e o dia no console
+        }
+    }
 }
